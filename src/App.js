a36c0aa4120b62e9054acd50bab341bce531cbd3
Jean Mark Kaboha <kabohajeanmark@gmail.com>
2025-01-08 15:26:09 +0300

    Set up react router

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
+import { BrowserRouter as Router, Routes,Route } from 'react-router-dom';
+
+import Home from './pages/Home';
 import './App.css';
 
 function App() {
   return (
     <div className="app">
       <h1>Google Clone</h1>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/search" element={<>This is the search page</>} />
+        </Routes>
+      </Router>
     </div>
   );
 }
---
+++ src/pages/Home.js
@@ -0,0 +1,28 @@
+import React from 'react'
+// import SearchIcon from '@mui/icons-material/Search';
+import './Home.css'
+
+function Home() {
+  return (
+    <div className='home'>
+      <h1>This is the Homepage</h1>
+      <div className="home__header">
+        <div className="header__left">
+          {/* <Link /> */}
+          {/* <Link /> */}
+        </div>
+        <div className="header__right">
+          {/* <Link /> */}
+          {/* <Link /> */}
+          {/* <Icon /> */}
+          {/* <Avatar /> */}
+        </div>
+      </div>
+      <div className="home__body">
+
+      </div>
+    </div>
+  )
+}
+
+export default Home
