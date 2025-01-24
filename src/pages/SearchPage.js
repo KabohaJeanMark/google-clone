38a439f6c2e12ed474a035915c72f17de2415fab
Jean Mark Kaboha <kabohajeanmark@gmail.com>
2025-01-24 13:11:41 +0300

    Use API keys for google custom search and created search engine

--- src/pages/SearchPage.js
+++ src/pages/SearchPage.js
@@ -1,10 +1,24 @@
 import React from 'react'
 import Search from '../components/Search'
+import { useStateValue } from '../contextAPI/StateProvider'
+import useGoogleSearch from '../useGoogleSearch'
+import '../styles/SearchPage.css'
 
 function SearchPage() {
+  const [{ term }, dispatch] = useStateValue();
+  const { data } = useGoogleSearch(term)
+
+  console.log(data)
+
   return (
-    <div>
+    <div className='searchPage'>
       <Search hideButtons/>
+      <div className="searchPage__header">
+        <h1>{term}</h1>
+      </div>
+      <div className="searchPage__results">
+
+      </div>
     </div>
   )
 }
