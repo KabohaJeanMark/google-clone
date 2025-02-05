cc513f321ccde4bbf05fb52e21127e95e5eb364d
Jean Mark Kaboha <kabohajeanmark@gmail.com>
2025-02-05 15:03:08 +0300

    remove the word key from .env variables for more security

--- src/useGoogleSearch.js
+++ src/useGoogleSearch.js
@@ -6,7 +6,7 @@ const useGoogleSearch = (term) => {
     useEffect(()=> {
         const fetchData = async() => {
             fetch(
-                `https://www.googleapis.com/customsearch/v1?key=${process.env.REACT_APP_GOOGLE_API_KEY}&cx=${process.env.REACT_APP_CONTEXT_KEY}&q=${term}`
+                `https://www.googleapis.com/customsearch/v1?key=${process.env.REACT_APP_GOOGLE_API}&cx=${process.env.REACT_APP_CONTEXT}&q=${term}`
             ).then(response => response.json())
             .then(result => {
                 setData(result)
