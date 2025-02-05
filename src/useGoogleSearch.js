9ec26f0dec58ff1382848958ceafa08e5ca3bd19
Jean Mark Kaboha <kabohajeanmark@gmail.com>
2025-02-05 14:53:21 +0300

    Fix issue of exposing api secret keys

--- src/useGoogleSearch.js
+++ src/useGoogleSearch.js
@@ -2,13 +2,11 @@ import { useState, useEffect } from "react";
 
 const useGoogleSearch = (term) => {
     const [data, setData] = useState(null);
-    const REACT_APP_GOOGLE_API_KEY = "AIzaSyDyJU5peNQE4iSQastsy9tu0pD4_35jll8";
-    const REACT_APP_CONTEXT_KEY = "a2428453afb33471d";
 
     useEffect(()=> {
         const fetchData = async() => {
             fetch(
-                `https://www.googleapis.com/customsearch/v1?key=${REACT_APP_GOOGLE_API_KEY}&cx=${REACT_APP_CONTEXT_KEY}&q=${term}`
+                `https://www.googleapis.com/customsearch/v1?key=${process.env.REACT_APP_GOOGLE_API_KEY}&cx=${process.env.REACT_APP_CONTEXT_KEY}&q=${term}`
             ).then(response => response.json())
             .then(result => {
                 setData(result)
